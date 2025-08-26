b50085bdc2bc11d3bcf4b0339c6cec00167b923e
kib <kib@SCIT-KIB>
2025-08-26 13:15:28 +0200

    Projektdateien hinzufügen.

---
+++ Kibke_Artur_Systemdaten_Overlay/Daten.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Management.Instrumentation;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+
+
+
+namespace Kibke_Artur_Systemdaten_Overlay
+{
+    internal class Daten
+    {
+        //Variable für den eingebauten RAM
+        public int Memory;
+
+        //Hier mit kriegt man den eingebauten RAM
+        [DllImport("kernel32.dll")]
+        public static extern bool GetPhysicallyInstalledSystemMemory(out long totalMemoryInKilobytes);
+
+        
+        //Das setzt die Instance cpuCounter und ramCounter um die Verschiedenen Werte zu holen
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
+        public Daten()
+        {
+            //Die Klammer gibt die CPU Daten wieder (Da kann auch was anderes stehen um Ram z.B. zu ziehen)
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            //RAM ziehen
+            ramCounter = new PerformanceCounter("Memory","Available MBytes");
+            
+            //Erste Abfrage immer leer
+            cpuCounter.NextValue(); 
+            ramCounter.NextValue();
+            
+
+            
+            //Warten
+            Thread.Sleep(500);
+            
+            //Liefert den Wert der Eingebauten RAM Platine
+           if (GetPhysicallyInstalledSystemMemory(out long totalMemoryKB))
+           {
+                //Hier wird der von einem long in ein int Convertiert und ausgerechnet
+                Memory = (int)(totalMemoryKB / 1024 / 1024);
+           } 
+        }
+
+        //Holt sich die IPAdresse
+        public string GetLocalIPAddress()
+        {
+            //Hier wird die IP vom host gespeichert in dem er den Name des host kennt
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            //Check der IPv4
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        
+        //Holt sich den Ping
+        public int Ping()
+        {
+            //Neue ping Instance
+            Ping ping1 = new Ping();
+
+            //Hier wird der ping gesendet zu 8.8.8.8(Google.de kann man auch hinschreiben)
+            //und mit Reply wird der vorgang gespeichert
+            PingReply ping = ping1.Send("8.8.8.8");
+            
+            //Hier werden die Millisekunden gezählt
+            return (int)ping.RoundtripTime;
+        }
+
+        public float CpuNutzung()
+        {    
+            //Liefert CPU Wert
+            return cpuCounter.NextValue(); 
+        }
+
+        public float RamNutzung()
+        {
+            //Liefert RAM Wert
+            return ramCounter.NextValue();
+        }
+
+        //WMI (Funktioniert nur bei neuern GPUs)
+        public float GpuNutzung()
+        {
+            //Ausgegebener wert am ende
+            float lastValue = 0;
+
+            //Suche nach der GPU 
+            var searcher = new ManagementObjectSearcher("root\\CIMV2",
+                "SELECT * FROM Win32_PerfFormattedData_GPUPerformanceCounters_GPUEngine");
+
+            //Hier sucht er und zieht sich den Wert
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                string name = obj["Name"]?.ToString() ?? "";
+                if (name.Contains("engtype_3D")) //Die 3D-Engine der GPU
+                {
+                    lastValue += Convert.ToSingle(obj["UtilizationPercentage"]);
+                }
+            }
+            return lastValue;
+        }
+
+        public int AkkuProzent()
+        {
+            //Das ist der Status für Akku aber ein PC hat kein weshalb da eigentlich
+            //immer 0 wäre aber mal 100 sind es 100% und in int umgewandelt
+            return (int)(SystemInformation.PowerStatus.BatteryLifePercent * 100);
+        }
+    }
+}
---
+++ Kibke_Artur_Systemdaten_Overlay/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+
+namespace Kibke_Artur_Systemdaten_Overlay
+{
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    {
+        //Objekten Liste wird gesetzt
+        ObservableCollection<Daten> systemDaten = new ObservableCollection<Daten>();
+
+        //neuen Timer gesetzt
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Fensterposition: oben rechts mit 10px Abstand
+            this.Left = SystemParameters.WorkArea.Width - this.Width - 10;
+            this.Top = 10;
+
+            if (!HatAkku())
+            {
+                AkkuRow.Visibility = Visibility.Collapsed;
+                this.Height -= 20;
+            }
+            
+            //Funktion zur Aktualisierung der CPU daten ablauf eines timers
+            StartTimer();
+        }
+
+        void StartTimer()
+        {
+            //Neues liste daten
+            Daten daten = new Daten();
+
+            //Timer wird gesetzt
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            //Wird nach ablauf des Timers Benutzt
+            timer.Tick += (s, e) =>
+            {
+                VerschiebeFensterAufZweitenMonitor();
+
+                //Klassen Funtkion wird auf gerufen und in TextBlock verfasst
+                CpuTextBlock.Text = $"{daten.CpuNutzung():F1}%";
+                GpuTextBlock.Text = $"{daten.GpuNutzung():F1}%";
+                RamTextBlock.Text = $"{daten.RamNutzung()*0.001:F1} / {daten.Memory:F1} GB";
+                AkkuTextBlock.Text = $"{daten.AkkuProzent()}%";
+                IpTextBlock.Text = $"{daten.GetLocalIPAddress()}";
+                PingTextBlock.Text = $"{daten.Ping()} ms";
+
+            };
+
+            //Timer wird wiederholt
+            timer.Start();
+        }
+
+        private void VerschiebeFensterAufZweitenMonitor()
+        {
+            var screens = Screen.AllScreens;
+
+            if (screens.Length > 1)
+            {
+                // Zweiten Monitor auswählen
+                var monitor = screens[1];
+
+                // Fensterposition setzen
+                this.Left = monitor.WorkingArea.Right - this.Width - 10;
+                this.Top = monitor.WorkingArea.Top + 10;
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Nur ein Monitor erkannt.");
+            }
+        }
+
+        private bool HatAkku()
+        {
+            var status = System.Windows.Forms.SystemInformation.PowerStatus.BatteryChargeStatus;
+            return status != System.Windows.Forms.BatteryChargeStatus.NoSystemBattery;
+        }
+    }
+}
